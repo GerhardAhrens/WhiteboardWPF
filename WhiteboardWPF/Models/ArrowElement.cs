7472d34c1034264672b755993591caca00333b4c
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2026-08-09 19:34:54 +0200

    *

---
+++ WhiteboardWPF/Models/ArrowElement.cs
@@ -0,0 +1,11 @@
+﻿namespace WhiteboardWPF.Models
+{
+    public class ArrowElement
+    {
+        public Guid Id { get; set; } = Guid.CreateVersion7();
+
+        public Guid SourceId { get; set; }
+
+        public Guid TargetId { get; set; }
+    }
+}
